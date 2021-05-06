a2a0201e5ee02553d2f7bca3fa3edd34c12a45cf
Aayan <aayankhare23@gmail.com>
2021-05-05 23:46:37 -0700

    tts

---
+++ App.js
@@ -0,0 +1,54 @@
+import * as React from 'react';
+import { Text, View, StyleSheet, TextInput,TouchableOpacity } from 'react-native';
+import * as Speech from "expo-speech"
+
+// You can import from local files
+
+export default class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      text:"",
+    }
+  }
+  speechfunction = ()=>{
+    var thingtoSay = this.state.text;
+    Speech.speak(thingtoSay)
+  }
+  render(){
+  return (
+    <View>
+      <TextInput style = {styles.inputBox} onChangeText= {(text)=>{
+        this.setState({text:text})
+      }}></TextInput>
+      <TouchableOpacity style = {styles.button} onPress = {()=>{
+        this.speechfunction();
+      }}>
+        <Text>Submit</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+}
+const styles = StyleSheet.create({
+  inputBox: {
+    borderColor: "green",
+    borderWidth: 4,
+    marginTop: 20,
+    padding: 20,
+    textAlign: "center",
+    width: "50%",
+    alignSelf: "center",
+  },
+  button:{
+    backgroundColor: "orange",
+    margin: 20,
+    padding: 10,
+    alignSelf: "center",
+    alignItems:"center",
+    borderRadius: 10,
+    width:100
+  }
+});
+
+
